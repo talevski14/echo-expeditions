22e143d433c9689f81a57f91fcb9dd5fb493a1e8
Nikola Talevski <talevskinikola1409@gmail.com>
2025-01-05 14:55:57 +0100

    add weapons

---
+++ Assets/Weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform FirePoint1;
+    public GameObject BulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(BulletPrefab, FirePoint1.position, FirePoint1.rotation);
+    }
+}
