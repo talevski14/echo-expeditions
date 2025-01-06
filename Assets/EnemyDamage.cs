f3a0cd1f28e7b65d18954e8ccda63d1861cb07df
petarpetreski24 <petarpetreski24@gmail.com>
2025-01-06 22:29:15 +0100

    Handling damage controll

---
+++ Assets/EnemyDamage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int health = 100;
+    public GameObject deathEffect;
+    public void Damaged(int damage)
+    {
+        health -= damage;
+        if (health <= null)
+        {
+            Instantiate(deathEffect, transform.position, Quaternion.identity);
+        }
+
+    }
+}
